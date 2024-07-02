f04cf4040e64593b0a4c0fc50a1eb1e770092856
Snehil Chatterjee <snehilchatterjee07@gmail.com>
2024-07-02 07:20:00 +0530

    tinySRGAN added

--- correct_app.py
+++ correct_app.py
@@ -137,6 +137,41 @@ class MiniSRGAN(nn.Module):
         
         return x, feat
 
+class TinySRGAN(nn.Module):
+    
+    def __init__(self, img_feat = 3, n_feats = 32, kernel_size = 3, num_block = 6, act = nn.PReLU(), scale=4):
+        super(TinySRGAN, self).__init__()
+        
+        self.conv01 = conv(in_channel = img_feat, out_channel = n_feats, kernel_size = 9, BN = False, act = act)
+        
+        resblocks = [ResBlock(channels = n_feats, kernel_size = 3, act = act) for _ in range(num_block)]
+        self.body = nn.Sequential(*resblocks)
+        
+        self.conv02 = conv(in_channel = n_feats, out_channel = n_feats, kernel_size = 3, BN = True, act = None)
+        
+        if(scale == 4):
+            upsample_blocks = [Upsampler(channel = n_feats, kernel_size = 3, scale = 2, act = act) for _ in range(2)]
+        else:
+            upsample_blocks = [Upsampler(channel = n_feats, kernel_size = 3, scale = scale, act = act)]
+
+        self.tail = nn.Sequential(*upsample_blocks)
+        
+        self.last_conv = conv(in_channel = n_feats, out_channel = img_feat, kernel_size = 3, BN = False, act = nn.Tanh())
+        
+    def forward(self, x):
+        
+        x = self.conv01(x)
+        _skip_connection = x
+        
+        x = self.body(x)
+        x = self.conv02(x)
+        feat = x + _skip_connection
+        
+        x = self.tail(feat)
+        x = self.last_conv(x)
+        
+        return x, feat
+
 
 def build_generator():
     
@@ -298,9 +333,9 @@ def translate_image(image, sharpen, model_name, save):
         output = output.transpose(1, 2, 0)
         sr_img = Image.fromarray((output * 255.0).astype(np.uint8))
         
-    elif(model_name=='MiniSRResNET'):
-        model = MiniSRGAN().to(device)
-        model.load_state_dict(torch.load('./weights/miniSRResNET.pt'))
+    elif(model_name=='TinySRGAN'):
+        model = TinySRGAN().to(device)
+        model.load_state_dict(torch.load('./weights/tinySRGAN.pt'))
         
         inputs = np.array(resized_image)
         inputs = (inputs / 127.5) - 1.0   
@@ -339,7 +374,7 @@ interface = gr.Interface(
     inputs=[
         gr.Image(type="pil"),
         gr.Checkbox(label="Sharpen Image"),
-        gr.Radio(choices=["MobileSR", "MiniSRGAN", "MiniSRResNET"], label="Select Model", value="MobileSR"),
+        gr.Radio(choices=["MobileSR", "MiniSRGAN", "TinySRGAN"], label="Select Model", value="MobileSR"),
         gr.Radio(choices=["True", "False"], label="Save Output", value="False")
     ],
     outputs=gr.Image(type="pil", label="Translated Image"),
